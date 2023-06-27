17d4d6f0bd2b85b6ee19db7a22bc1b988db05be5
sourajeet_dey <write2sjdey@gmail.com>
2023-06-28 03:27:12 +0530

    Addition of examples

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,18 +1,172 @@
 const medicines = [
   {
-    name: "Advil",
-    uses: ["Pain relief", "Fever reducer"],
-    side_effects: ["Stomach upset", "Diarrhea"],
+    name: "RL",
+    uses: [
+      "Dehydration",
+      "Extra fluid & electrolyte during surgery and trauma",
+    ],
+    side_effects: [
+      "Cardiac toxicity in rapid infusion",
+      "Chest pain",
+      "Black stool",
+    ],
   },
   {
-    name: "Tylenol",
-    uses: ["Pain relief", "Fever reducer"],
-    side_effects: ["Nausea", "Dizziness"],
+    name: "Injection Lignocine",
+    uses: [
+      "Anasthetic in surgery",
+      "Reduce pain of invasive procedure",
+      "Ventricular Arrythmias",
+    ],
+    side_effects: ["Blurred vision", "Headache", "Convulsion"],
   },
   {
-    name: "Aleve",
-    uses: ["Pain relief", "Fever reducer"],
-    side_effects: ["Stomach upset", "Headache"],
+    name: "Injection Pipzo",
+    uses: [
+      "Pseudomonas Bacteremias",
+      "Treat intra-abdominal infection and peritonitis",
+      "Treat UTI",
+    ],
+    side_effects: ["Constipation", "Diarrhea", "Headache", "Nausea"],
+  },
+  {
+    name: "Duolin",
+    uses: [
+      "Treat Chronic Obstructive Pulmonary Disorder",
+      "Used as bronchodilator",
+      "To relieve shortness of breathing",
+    ],
+    side_effects: ["Dryness of mouth", "Cough", "Headache"],
+  },
+  {
+    name: "Injection KCl",
+    uses: [
+      "Treat hypokalemia",
+      "Used in hypokalemic prophylaxis",
+      "To increase potassium level in body",
+    ],
+    side_effects: ["Hyperkalemia", "Diarrhea", "Abdominal pain"],
+  },
+  {
+    name: "Betadine Solution",
+    uses: [
+      "As antiseptic",
+      "Used to stop microbial growth",
+      "To treat or prevent microbial growth",
+    ],
+    side_effects: ["Redness", "Irritation"],
+  },
+  {
+    name: "Atropine",
+    uses: [
+      "To treat Bradycardia",
+      "Second line treatment for diarrhea",
+      "To reduce salivation and bronchospasm during surgery",
+    ],
+    side_effects: [
+      "Redness & Irritation in eyes",
+      "Blurred vision",
+      "Tachycardia",
+    ],
+  },
+  {
+    name: "Injection Vincristine Sulphate",
+    uses: [
+      "To treat certain childhood tumors",
+      "In chemotherapy",
+      "To treat leukemia",
+    ],
+    side_effects: [
+      "Peripheral neuritis",
+      "Constipation",
+      "Headache",
+      "Vomiting",
+    ],
+  },
+  {
+    name: "Sodium Phosphate Enema",
+    uses: [
+      "Laxative to relieve constipation",
+      "To prepare patient for colonoscopy",
+      "To treat leukemia",
+    ],
+    side_effects: ["Mild abdominal cramp", "Gas formation"],
+  },
+  {
+    name: "Cisplatin",
+    uses: [
+      "To treat cancer",
+      "To treat solid tumors",
+      "To treat hematologic malignancies",
+    ],
+    side_effects: ["Loss of taste", "Fatigue", "Loss of hearing"],
+  },
+  {
+    name: "Injection Levetiracetam",
+    uses: [
+      "To treat partial-onset seizures",
+      "To treat epilepsy",
+      "To treat convulsion caused by brain tumors",
+    ],
+    side_effects: ["Convulsions", "Dizziness", "Headache", "Weakness"],
+  },
+  {
+    name: "Tranexamic acid",
+    uses: [
+      "To reduce heavy menstrual bleeding",
+      "To reduce bleeding in surgery",
+      "To treat prostate cancer",
+    ],
+    side_effects: ["Joint Pain", "Muscle pain", "Headache"],
+  },
+  {
+    name: "Dexamethasone",
+    uses: [
+      "To reduce inflammation",
+      "Used in Arthritis",
+      "Used in asthma and allergy",
+    ],
+    side_effects: ["Loss of appetite", "Indigestion", "Sleep Problem"],
+  },
+  {
+    name: "Telmisartan",
+    uses: [
+      "Used in hypertension",
+      "Used in heart failure, Myocardial infarction",
+    ],
+    side_effects: ["Tachycardia", "Headache", "Hypertension"],
+  },
+  {
+    name: "PCM",
+    uses: [
+      "To treat fever",
+      "To treat abdominal pain",
+      "Hay fever, cold, running nose",
+    ],
+    side_effects: ["Hepatotoxicity", "Stomach upset", "Blurred vision"],
+  },
+  {
+    name: "Metrogyl",
+    uses: ["Amoebiasis", "Bacterial septicemia", "Bacterial infection"],
+    side_effects: ["Stomach pain", "Headaches", "Hot flushes"],
+  },
+  {
+    name: "Amoxyclav",
+    uses: [
+      "To treat UTI",
+      "Middle ear and sinus infection",
+      "Throat infection",
+    ],
+    side_effects: ["Skin rash", "Wheezing", "Diarrhea", "Tightness of chest"],
+  },
+  {
+    name: "Drotin",
+    uses: [
+      "In menstrual cramps",
+      "Abdominal pain",
+      "Kidney stone & gallbladder stone pain",
+    ],
+    side_effects: ["Nausea", "Vomiting", "Weakness"],
   },
 ];
 
